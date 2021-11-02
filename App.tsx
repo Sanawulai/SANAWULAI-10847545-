9a02a83e2a0d92ba77bb9ba6a179862cd423ccdd
sanawulai <numbonayar@gmail.com>
2021-11-02 14:21:17 +0000

    init

--- App.tsx
+++ App.tsx
@@ -1,21 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React from 'react';
+
+
+//screens
+import Login from './screens/Login';
+import Signup from './screens/Signup';
+export default function App(){
+    return<Signup/>;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'indigo',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
