a3274cdc68933f276847e23b772bee266a75d9bb
sanawulai <numbonayar@gmail.com>
2021-11-17 15:51:49 +0000

    init

--- .vscode/constants/colors.js
+++
@@ -1,4 +0,0 @@
-export default {
-    primary: "#02538C",
-    accent: "#069988"
-  };
--- App.tsx
+++ App.tsx
@@ -2,11 +2,13 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
-
+import {Products, Cart, Checkout } from './components/components';
 
 const Stack= createStackNavigator();
 import Homepage from './screens/Homepage';
 import productsDetails from './consts/ProductsDetails';
+
+
 
 
 
